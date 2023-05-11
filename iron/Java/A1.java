d3f7f842b6e9975ba2d8e6292fff164ac8fd0872
kaneda05 <kanemasa1541km@gmail.com>
2023-05-11 17:22:30 +0900

    finish

---
+++ iron/Java/A1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+        System.out.println(N*N);
+    }
+}
