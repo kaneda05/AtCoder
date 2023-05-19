1620065cdd38234c23491e033d9a578e411794ea
kaneda05 <kanemasa1541km@gmail.com>
2023-05-19 19:39:39 +0900

    move

---
+++ iron/chr8/Python/A51.py
@@ -0,0 +1,14 @@
+from collections import deque
+Q = int(input())
+querys = [input().split() for _ in range(Q)]
+
+D = deque()
+for q in querys:
+    if q[0] == "1":
+        D.append(q[1])
+
+    if q[0] == "2":
+        print(D[-1])
+
+    if q[0] == "3":
+        D.pop()
