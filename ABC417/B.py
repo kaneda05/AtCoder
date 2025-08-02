cad2560aa92bab8fb3215a621a0331d7086f8940
kaneda05 <kanemasa1541km@gmail.com>
2025-08-02 23:07:16 +0900

    solve

---
+++ ABC417/B.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+for b in B:
+    for i in range(len(A)):
+        if A[i] == b:
+            A.pop(i)
+            break
+print(*A)
